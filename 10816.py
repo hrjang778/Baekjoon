60b8d3a86293aeca55be4eef8cd126a5debaf15e
장형렬 <hrjang@inzisoft.com>
2024-07-05 10:40:38 +0900

    문제 해결 딕셔너리, count.get(i) 사용

--- 10816.py
+++ 10816.py
@@ -1,9 +1,23 @@
 import sys
 input = sys.stdin.readline
+count = {}
 N = int(input())
-comparedCard = list(map(int,input().split()))
+comparedCard = [*map(int,input().split())]
 M = int(input())
-compareCard = list(map(int,input().split()))
+compareCard = [*map(int,input().split())]
+for i in comparedCard:
+    if i in count:
+        count[i] += 1
+    else:
+        count[i] = 1
+
 for i in compareCard:
-    print(comparedCard.count(i),end=' ')
-# 딕셔너리 이용해 볼것
+    if count.get(i) == None:
+        print(0, end = " ")
+    else:
+        print(count.get(i), end = " ")
+# count[i] 보다는 count.get(i)를 사용하기 -> count[i]는 KeyError를 유발하기 때문
+# 반면 count.get(i)는 값이 없으면 None 값을 리턴
+# 또한 리스트 보다 딕셔너리가 속도가 빠르다
+# 리스트 -> O(N)
+# 딕셔너리 -> O(1)
