396fe0a11b09e2dfffdbf7c6686dc087437d7ee1
장형렬 <hrjang@inzisoft.com>
2024-07-05 14:38:12 +0900

    문제 조건을 잘 읽자! 조건이 생각보다 많았음

--- 1764.py
+++ 1764.py
@@ -1,9 +1,13 @@
 import sys
 input = sys.stdin.readline
 N,M = map(int,input().split())
-unListen = [input().strip() for i in range(N)]
-unLook = [input().strip() for i in range(N)]
+unListen = set([input().strip() for i in range(N)])
+unLook = set([input().strip() for i in range(M)])
 duplication = []
 for i in unListen:
     if i in unLook:
         duplication.append(i)
+duplication.sort()
+print(len(duplication))
+for i in duplication:
+    print(i)
